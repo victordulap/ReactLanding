2693f7c8a2d9e9842bcf20930360a784b1565407
Victor Dulap <anunachi1337@gmail.com>
2021-08-17 14:46:44 +0300

    added navbar and hero components

---
+++ src/components/pages/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../../App.scss';
+import Hero from '../Hero';
+
+const Home = () => {
+  return (
+    <>
+      <Hero />
+    </>
+  );
+};
+
+export default Home;
